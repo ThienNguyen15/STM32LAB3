c0c58587ec177caf3772f80f1dfc195a9f5c2851
LAPTOP-HH2P921E\thien <nguyenquangthien1506>
2023-10-21 15:31:33 +0700

    Init for Ex2

--- Exercises/Debug/Core/Src/subdir.mk
+++ Exercises/Debug/Core/Src/subdir.mk
@@ -5,46 +5,28 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../Core/Src/blinky_led.c \
-../Core/Src/button.c \
-../Core/Src/global.c \
-../Core/Src/input_processing.c \
-../Core/Src/input_reading.c \
 ../Core/Src/main.c \
 ../Core/Src/stm32f1xx_hal_msp.c \
 ../Core/Src/stm32f1xx_it.c \
 ../Core/Src/syscalls.c \
 ../Core/Src/sysmem.c \
-../Core/Src/system_stm32f1xx.c \
-../Core/Src/timer.c 
+../Core/Src/system_stm32f1xx.c 
 
 OBJS += \
-./Core/Src/blinky_led.o \
-./Core/Src/button.o \
-./Core/Src/global.o \
-./Core/Src/input_processing.o \
-./Core/Src/input_reading.o \
 ./Core/Src/main.o \
 ./Core/Src/stm32f1xx_hal_msp.o \
 ./Core/Src/stm32f1xx_it.o \
 ./Core/Src/syscalls.o \
 ./Core/Src/sysmem.o \
-./Core/Src/system_stm32f1xx.o \
-./Core/Src/timer.o 
+./Core/Src/system_stm32f1xx.o 
 
 C_DEPS += \
-./Core/Src/blinky_led.d \
-./Core/Src/button.d \
-./Core/Src/global.d \
-./Core/Src/input_processing.d \
-./Core/Src/input_reading.d \
 ./Core/Src/main.d \
 ./Core/Src/stm32f1xx_hal_msp.d \
 ./Core/Src/stm32f1xx_it.d \
 ./Core/Src/syscalls.d \
 ./Core/Src/sysmem.d \
-./Core/Src/system_stm32f1xx.d \
-./Core/Src/timer.d 
+./Core/Src/system_stm32f1xx.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
