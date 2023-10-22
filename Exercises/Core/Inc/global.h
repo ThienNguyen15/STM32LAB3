c3f52510169fad903eebef31dc3d84bfa3a8b361
LAPTOP-HH2P921E\thien <nguyenquangthien1506>
2023-10-23 00:21:37 +0700

    Init for Ex6

--- Exercises/Core/Inc/global.h
+++ Exercises/Core/Inc/global.h
@@ -16,18 +16,18 @@
 
 #define NORMAL_STATE	GPIO_PIN_SET
 #define PRESSED_STATE 	GPIO_PIN_RESET
-#define BUTTONS		3
 #define BUTTON_1	0
 #define BUTTON_2	1
 #define BUTTON_3	2
+#define BUTTONS		3
+
+#define TIMER		4
 
 #define MODE_INIT	8
 #define MODE_1		9
 #define MODE_2		10
 #define MODE_3		11
 #define MODE_4		12
-
-#define TIMER		4
 
 #define LED_ON			GPIO_PIN_SET
 #define LED_OFF			GPIO_PIN_RESET
